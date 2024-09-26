dc52f7e282e30079721fbbeb12bcf781c7d42152
iJoe42 <yts.ijoe.wss@gmail.com>
2024-09-26 13:19:25 +0700

    finished
    tutorial finished

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,21 +9,23 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  unstable_noStore();
 
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+     console.log('Fetching revenue data...');
+     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+     console.log('Revenue fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -33,13 +35,20 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  unstable_noStore();
   try {
+
+     console.log('Fetching latestInvoices data...');
+     await new Promise((resolve) => setTimeout(resolve, 5000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+     console.log('latestInvoices fetch completed after 5 seconds.');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
@@ -53,16 +62,23 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  unstable_noStore();
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
+
+     console.log('Fetching cardData data...');
+     await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
+
+     console.log('cardData fetch completed after 2 seconds.');
 
     const data = await Promise.all([
       invoiceCountPromise,
@@ -124,6 +140,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  unstable_noStore();
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -145,6 +162,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  unstable_noStore();
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -170,6 +188,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
+  unstable_noStore();
   try {
     const data = await sql<CustomerField>`
       SELECT
@@ -188,6 +207,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  unstable_noStore();
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
